65fa55e3d66964fe90304925fa8f6c968b8adf01
bilalzahran <bilalzahran08@gmail.com>
2021-11-02 21:48:35 +0700

    chore: add ddl sql file

---
+++ ddl.sql
@@ -0,0 +1,46 @@
+CREATE TABLE "employees" (
+    "id" serial PRIMARY KEY,
+    "name" varchar,
+    "username" varchar,
+    "password" varchar,
+    "gender" varchar,
+    "birthdate" date
+);
+
+CREATE TABLE "doctors" (
+    "id" serial PRIMARY KEY,
+    "name" varchar,
+    "username" varchar,
+    "password" varchar,
+    "gender" varchar,
+    "birthdate" date,
+    "work_start_time" datetime,
+    "work_end_time" datetime
+);
+
+CREATE TABLE "patients" (
+    "id" serial PRIMARY KEY,
+    "name" varchar,
+    "gender" varchar,
+    "birthdate" varchar,
+    "no_ktp" varchar,
+    "address" text,
+    "vaccine_type" varchar,
+    "vaccine_count" varchar
+);
+
+CREATE TYPE appointment_status as ENUM ("IN_QUEUE","DONE","CANCELLED");
+CREATE TABLE "appointments" (
+    "id" serial PRIMARY KEY,
+    "patient_id" int,
+    "doctor_id" int,
+    "datetime" datetime,
+    "status" appointment_status,
+    "diagnose" text,
+    "notes" text
+);
+
+ALTER TABLE "appointments"
+ADD FOREIGN KEY ("patient_id") REFERENCES "patients" ("id");
+ALTER TABLE "appointments"
+ADD FOREIGN KEY ("doctor_id") REFERENCES "doctors" ("id");
